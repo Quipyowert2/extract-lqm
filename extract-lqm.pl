c4bf4e7213e5f82455f43e295f820ecad4cee327
Quipyowert2 <38995150+Quipyowert2@users.noreply.github.com>
2019-11-15 18:28:56 -0800

    Add usage message.

--- extract-lqm.pl
+++ extract-lqm.pl
@@ -6,6 +6,15 @@ use Archive::Zip;
 use Archive::Zip::MemberRead;
 use JSON;
 use Data::Dumper;
+if (!@ARGV) {
+    print STDERR<<USAGE;
+extract-lqm.pl <.LQM file>
+Extract text from .LQM (QuickMemo+) files
+Outputs the resulting text on stdout.
+Put '>output.txt' after the LQM file to send output to a file named output.txt
+USAGE
+    exit 1;
+}
 my $lqm = shift @ARGV;
 my $zip = Archive::Zip->new($lqm);
 my $fh = Archive::Zip::MemberRead->new($zip, "memoinfo.jlqm");
